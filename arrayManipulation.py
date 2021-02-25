7e9facbce51823f8ee716c3358535ed7f4c02e70
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-02-25 10:54:54 -0500

    added matrix in spiral order

--- arrayManipulation.py
+++ arrayManipulation.py
@@ -99,8 +99,27 @@ def nonuniform_random_number_generation(values, probabilities):
     return values[interval_idx]
 
 
+def matrix_in_spiral_order(square_matrix):
+    SHIFT = ((0,1),(1,0),(0,-1),(-1,0))
+    direction = x = y = 0
+    spiral_ordering = []
+
+    for _ in range(len(square_matrix) ** 2):
+        spiral_ordering.append(square_matrix[x][y])
+        square_matrix[x][y] = 0
+        next_x, next_y = x + SHIFT[direction][0], y + SHIFT[direction][1]
+        if (next_x not in range(len(square_matrix))
+        or next_y not in range(len(square_matrix))
+        or square_matrix[next_x][next_y] == 0):
+            direction = (direction + 1) & 3
+            next_x, next_y = x + SHIFT[direction][0], y + SHIFT[direction][1]
+        x, y = next_x, next_y
+    return spiral_ordering
+
+
 
 if __name__ == "__main__":
-    for i in range(5):
-        print(nonuniform_random_number_generation([3,5,7,11], [9/18,6/18,2/18,1/18] ))
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+    print(matrix_in_spiral_order(matrix))
     
