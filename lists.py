9145ecb1ffcd29c63e952a37d402ec2f538cf6bd
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-03 16:44:47 -0500

    added merge to sorted lists

--- lists.py
+++ lists.py
@@ -24,3 +24,18 @@ def delete_after(node):
     Delete the node past this one. Assume node is not a tail
     """
     node.next = node.next.next
+
+def merge_two_sorted_lists(L1, L2):
+    # creates a placeholder for the result
+
+    dummy_head = tail = ListNode()
+
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next, L1 = L1, L1.next
+        else:
+            tail.next, L2 = L2, L2.next
+        tail = tail.next
+    # append remaining nodes of L1 or L2
+    tail.next = L1 or L2
+    return dummy_head.next 
