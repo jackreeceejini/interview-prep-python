69e68c8f7826bae22f123831976fae28708c371a
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-04 11:45:58 -0500

    added overlapping no cycle lists

--- lists.py
+++ lists.py
@@ -81,3 +81,23 @@ def has_cycle(head):
                 cycle_len_advanced_iter = cycle_len_advanced_iter.next
             return it # iter is the start of cycle
     return None # No cycle
+
+
+def overlapping_no_cycle_lists(L1, L2):
+    def length(L):
+        length = 0
+        while L:
+            length += 1
+            L = L.next
+        return length
+
+    L1_len, L2_len = length(L1), length(L2)
+    if L1_len > L2_len:
+        L1, L2 = L2, L1 # L2 is the longer list
+    #Advances the longer list to get equal length lists.
+    for _ in range(abs(L1_len - L2_len)):
+        L2 = L2.next
+    
+    while L1 and L2 and L1 is not L2:
+        L1, L2 = L1.next, L2.next
+    return L1 #None implies there is no overlap between L1 and L2
