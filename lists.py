4dfb7596b7274ac33df996307271f7f9dde21bb6
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-05 13:40:10 -0500

    added cyclic shift right by k

--- lists.py
+++ lists.py
@@ -134,3 +134,30 @@ def remove_duplicates(L):
         it.next = next_distinct
         it = next_distinct
     return L 
+
+def cyclic_shift_right(L, k):
+    if not L:
+        return L
+    
+    # computes the length of L and the tail
+    tail, n = L, 1
+    while tail.next:
+        n += 1
+        tail = tail.next
+    
+    k %= n
+    if k == 0:
+        return L
+    
+    tail.next = L # makes a cycle by connecting the tail to the head
+    steps_to_new_head, new_tail = n - k, tail
+    while steps_to_new_head:
+        steps_to_new_head -= 1
+        new_tail = new_tail.next
+    
+    new_head = new_tail.next
+
+    new_tail.next = None
+    return new_head 
+    
+        
