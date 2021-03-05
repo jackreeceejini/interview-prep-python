a00bc980badf3158d365541d27c4263f2f9ea1c2
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-05 15:49:32 -0500

    added even odd merge

--- lists.py
+++ lists.py
@@ -166,4 +166,13 @@ def even_odd_merge(L):
         return L
 
     even_dummy_head, odd_dummy_head = ListNode(0), ListNode(0)
+    tails, turn = [even_dummy_head, odd_dummy_head], 0
+    while L:
+        tails[turn].next = L
+        L = L.next
+        tails[turn] = tails[turn].next 
+        turn ^= 1 # Alternate between even and odd
+    tails[1].next = None
+    tails[0].next = odd_dummy_head.next 
+    return even_dummy_head
         
