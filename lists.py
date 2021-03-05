a10da48f410cada5be349b352a4dbab16c30fde0
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-05 12:56:09 -0500

    added remove duplicates

--- lists.py
+++ lists.py
@@ -122,3 +122,15 @@ def remove_kth_last(L, k):
     # second points to the (k + 1)-th last node, deletes its successor
     second.next = second.next.next
     return dummy_head.next 
+
+
+def remove_duplicates(L):
+    it = L
+    while it:
+        # uses next_distinct to find the next distinct value
+        next_distinct = it.next
+        while next_distinct and next_distinct.data == it.data:
+            next_distinct = next_distinct.next
+        it.next = next_distinct
+        it = next_distinct
+    return L 
