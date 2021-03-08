e369e935adc761baff2171896742273d1c41dd15
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-08 11:08:48 -0500

    add is linked list a palindrome

--- lists.py
+++ lists.py
@@ -175,4 +175,19 @@ def even_odd_merge(L):
     tails[1].next = None
     tails[0].next = odd_dummy_head.next 
     return even_dummy_head
+
+def is_linked_list_a_palindrome(L):
+    # Finds the second half of L
+    slow = fast = L
+    while fast and fast.next:
+        slow, fast = slow.next, fast.next.next
+
+    # Compares the first half and the reversed second half lists
+    first_half_iter, second_half_iter = L, reversed_linked_list(slow)
+    while second_half_iter and first_half_iter:
+        if second_half_iter.data != first_half_iter.data:
+            return False
+        second_half_iter, first_half_iter = (second_half_iter.next, first_half_iter.next)
+    return True
+
         
