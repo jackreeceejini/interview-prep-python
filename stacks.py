d5166b68c224ba208a99d4cc1df5292194219395
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-08 14:10:44 -0500

    added print LL in reverse

---
+++ stacks.py
@@ -0,0 +1,11 @@
+"""
+Stack manipulations
+"""
+
+def print_linked_list_in_reverse(head):
+    nodes = []
+    while head:
+        nodes.append(head.data)
+        head = head.next
+    while nodes:
+        print(nodes.pop())
