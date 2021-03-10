929af59cfc95295858e655588ec92135e81f1200
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-10 11:21:18 -0500

    added shortest equivalent path

--- stacks.py
+++ stacks.py
@@ -69,6 +69,30 @@ def balanced(inputString):
             return False
     return not leftParens
 
+def shortest_equivalent_path(path):
+    if not path:
+        raise ValueError('Empty string is not a valid path.')
+
+    path_names = [] #uses list as stack
+
+    #special case: starts with '/', which is an absolute path
+
+    if path[0] == '/':
+        path_names.append('/')
+    
+    for token in (token for token in path.split('/') if token not in ['.','']):
+        if token == '..':
+            if not path_names or path_names[-1] == '..':
+                path_names.append(token)
+            else:
+                if path_names[-1] == '/':
+                    raise ValueError('Path error')
+                path_names.pop()
+        else: # must be a name
+            path_names.append(token)
+
+    result = '/'.join(path_names)
+    return result[result.startswith('//'):] # Avoid starting with '//
 
 if __name__ == '__main__':
     print(evaluate("3,4,+,2,*,1,+"))
