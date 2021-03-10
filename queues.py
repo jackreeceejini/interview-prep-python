8ccab665aebc97f2cbb7e64b3691ed5b902dd80c
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-10 15:46:48 -0500

    added binary tree depth order

--- queues.py
+++ queues.py
@@ -15,3 +15,17 @@ class Queue:
     def max(self):
         return max(self._data)
 
+def binary_tree_depth_order(tree):
+    """
+    Traverse a binary tree in depth order return 
+    nodes at each level from root to leaves
+    """
+    results = []
+    if not tree:
+        return results
+    
+    current_children = [tree]
+    while current_children:
+        results.append([curr.data for curr in current_children])
+        current_children = [child for curr in current_children for child in (curr.left, curr.right) if child]
+    return results 
