948ed0d5f3fe216fe3d9ecb87aa910e5d0d12201
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-16 11:08:38 -0400

    added dequeue, size

--- queues.py
+++ queues.py
@@ -54,5 +54,16 @@ class Queue1:
         self._entries[self._tail] = x
         self._tail = (self._tail + 1) % len(self._entries)
         self._num_queue_elements += 1
+    
+    def dequeue(self):
+        if not self._num_queue_elements:
+            raise IndexError('empty queue')
+        self._num_queue_elements -= 1
+        ret = self._entries[self._head]
+        self._head = (self._head + 1) % len(self._entries)
+        return ret
+
+    def size(self):
+        return self._num_queue_elements 
 
     
