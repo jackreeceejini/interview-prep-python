6d4457cc878713f3ac1e8bf8fb29706c9ca3f6f2
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-16 12:21:36 -0400

    added Queue with max

--- queues.py
+++ queues.py
@@ -85,4 +85,29 @@ class Queue2:
         if not self._deq: # _deq is still empty
             raise IndexError('empty queue')
         return self._deq.pop()
-        
+
+
+class QueueWithMax:
+    def __init__(self):
+        self._entries = collections.deque()
+        self._candidates_for_max = collections.deque()
+
+    def enqueue(self, x):
+        self._entries.append(x)
+        # Eliminate dominated elements in _candidates_for_max
+        while self._candidates_for_max and self._candidates_for_max[-1] < x:
+            self._candidates_for_max.pop()
+        self._candidates_for_max.append(x)
+
+    def dequeue(self):
+        if self._entries:
+            result = self._entries.popleft()
+            if self._candidates_for_max[0] == result:
+                self._candidates_for_max.popleft()
+            return result
+        raise IndexError('Empty Queue')
+
+    def max(self):
+        if self._candidates_for_max:
+            return self._candidates_for_max[0]
+        raise IndexError('empty queue')
