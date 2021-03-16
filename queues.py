4e27a6da8f9c5b831a1f0f17596464c571c8bc2c
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-16 11:03:33 -0400

    added enqueue

--- queues.py
+++ queues.py
@@ -3,6 +3,8 @@
 import collections
 
 class Queue:
+    """A basic queue implementation
+    """
     def __init__(self) -> None:
         self._data = collections.deque()
 
@@ -29,3 +31,28 @@ def binary_tree_depth_order(tree):
         results.append([curr.data for curr in current_children])
         current_children = [child for curr in current_children for child in (curr.left, curr.right) if child]
     return results 
+
+
+class Queue1:
+    """
+    A queue a more sophisticated queue whose 
+    total size increase according to how full it is
+    """
+    SCALE_FACTOR = 2
+
+    def __init__(self, capacity) -> None:
+        self._entries = [None] * capacity
+        self._head = self._tail = self._num_queue_elements = 0
+
+    def enqueue(self, x):
+        if self._num_queue_elements == len(self._entries): # needs to resize
+            # makes the queue elements appear consecutively
+            self._entries(self._entries[self._head:] + self._entries[:self._head])
+            # Resets head and tail
+            self._head, self._tail = 0, self._num_queue_elements
+            self._entries += [None] * (len(self._entries) * Queue.SCALE_FACTOR - len(self._entries))
+        self._entries[self._tail] = x
+        self._tail = (self._tail + 1) % len(self._entries)
+        self._num_queue_elements += 1
+
+    
