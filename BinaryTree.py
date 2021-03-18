740fab7c23312a9b832ffb1eb06ec51835465915
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-18 15:35:15 -0400

    find kth node in binary tree

--- BinaryTree.py
+++ BinaryTree.py
@@ -127,3 +127,18 @@ def preorder_traversal(tree):
             curr += [curr.right, curr.left]
     return result
 
+def find_kth_node_binary_tree(tree, k):
+    while Tree:
+        left_size = tree.left.size if tree.left else 0
+        if left_size + 1 < k: # k-th node must be in right subtree of tree
+            k -= left_size + 1
+            tree = tree.right
+        elif left_size == k - 1: # k-th is iter itself
+            return tree
+        else: # k-th node must be in left subtree of iter
+            tree = tree.left
+
+    return None # if k is between 1 and the tree size, this is unreacheable
+
+    
+
