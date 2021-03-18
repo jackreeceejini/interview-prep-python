f9fe02d6be52e002cff4af435019ad11f6a4c2fe
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-18 14:36:31 -0400

    added sum root to leaf

--- BinaryTree.py
+++ BinaryTree.py
@@ -75,3 +75,12 @@ def lcaWithParent(node_0, node_1):
         node_0, node_1 = node_0.parent, node_1.parent 
     return node_0
 
+def sum_root_to_leaf(tree, partial_path_sum=0):
+    if not tree:
+        return 0
+
+    partial_path_sum = partial_path_sum * 2 + tree.data
+    if not tree.left and not tree.right: #leaf
+        return partial_path_sum
+    return (sum_root_to_leaf(tree.left, partial_path_sum) + sum_root_to_leaf(tree.right, partial_path_sum))
+    
