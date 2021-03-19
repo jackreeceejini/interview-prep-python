91e58e0e41708acc6d333a1af5a89e433537b25e
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-19 13:59:19 -0400

    added binary tree preorder inorder

--- BinaryTree.py
+++ BinaryTree.py
@@ -186,3 +186,28 @@ def inorder_traversal_O1space(tree):
 
         prev, tree = tree, next
     return result 
+
+def binary_tree_from_preorder_inorder(preorder, inorder):
+    """
+    Build a binary tree from preorder and inorder traversal data
+    """
+    node_to_inorder_idx = {data: i for i, data in enumerate(inorder)}
+
+    # Builds the subtree with preoder[preorder_start:preorder_end] and
+    # inorder[inorder_start:inorder_end]
+    def binary_tree_from_preorder_inorder_helper(preorder_start, preorder_end,inorder_start, inorder_end):
+        if preorder_end <= preorder_start or inorder_end <= inorder_start:
+            return None
+        root_inorder_idx = node_to_inorder_idx[preorder[preorder_start]]
+        left_subtree_size = root_inorder_idx - inorder_start
+        return BinaryTreeNode(
+            preorder[preorder_start],
+            # recursively builds the left subtree.
+            binary_tree_from_preorder_inorder_helper(preorder_start + 1, preorder_start + 1 + left_subtree_size, inorder_start, root_inorder_idx),
+            # recursively builds the right subtree.
+            binary_tree_from_preorder_inorder_helper(preorder_start + 1 + left_subtree_size, preorder_end, root_inorder_idx + 1, inorder_end)
+
+        )
+    return binary_tree_from_preorder_inorder_helper(0, len(preorder), 0, len(inorder))
+
+
