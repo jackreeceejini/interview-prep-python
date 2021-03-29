2354947f902fccf6adfbb55f6a9ffdd43f907b24
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-03-29 15:46:02 -0400

    added closest k stars

--- heaps.py
+++ heaps.py
@@ -1,5 +1,6 @@
 import itertools
 import heapq
+import math
 
 def top_k(k, stream):
     # Entries are compared by thier lengths
@@ -51,5 +52,28 @@ def sort_approximately_sorted_array(sequence, k):
     while min_heap:
         smallest = heapq.heappop(min_heap)
         result.append(smallest)
-        
+
     return result
+
+
+class Star:
+    def __init__(self, x, y, z) -> None:
+        self.x, self.y, self.z = x, y, z 
+
+    @property
+    def distance(self):
+        return math.sqrt(self.x**2, self.y**2, self.z**2)
+
+    def __lt__(self, rhs):
+        return self.distance < rhs.distance
+
+
+def find_closest_k_stars(k, stars):
+    max_heap = []
+    for star in stars:
+        heapq.heappush(max_heap, (-star.distance, star))
+        if len(max_heap == k + 1):
+            heapq.heappop(max_heap)
+    
+    return [s[1] for s in heapq.nlargest(k, max_heap)]
+        
