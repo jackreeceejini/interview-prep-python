dcc1cfdad9098ba0fa95d8c537d8556121fefeab
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-04-02 13:28:40 -0400

    added search entry equal to its index

--- search.py
+++ search.py
@@ -31,6 +31,23 @@ def search_first_k(A, k):
     
     return result
 
+def searh_entry_equal_to_its_index(A):
+    """ Search for first occurence of k
+    """
+    left, right = 0, len(A) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        difference = A[mid] - mid
+        # A[mid] == mid if and only if difference == 0
+        if difference ==  0:
+            return mid
+        elif difference > 0:
+            right = M - 1
+        else:
+            left = M + 1
+    
+    return -1
 if __name__ == "__main__":
     data = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
     print(search_first_k(data, 285))
