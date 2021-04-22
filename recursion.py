39c18374749c0f394394d1c974471787e48a204d
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2021-04-22 10:04:56 -0400

    added generate balanced parentheses

--- recursion.py
+++ recursion.py
@@ -28,10 +28,19 @@ def generate_power_set(S):
         power_set.append(subset)
     return power_set
 
+def generate_balanced_parentheses(num_pairs):
+    def directed_generate_balanced_parentheses(num_left_parens_needed, num_right_parens_needed, valid_prefix, result=[]):
+        if num_left_parens_needed > 0: # able to insert '('.
+            directed_generate_balanced_parentheses(num_left_parens_needed - 1, num_right_parens_needed, valid_prefix + '(')
+        if num_left_parens_needed < num_right_parens_needed:
+            # Able to insert ')'.
+            directed_generate_balanced_parentheses(num_left_parens_needed, num_right_parens_needed - 1, valid_prefix + ')')
+        if not num_right_parens_needed:
+            result.append(valid_prefix)
+        return result 
+    return directed_generate_balanced_parentheses(num_pairs, num_pairs, '')
 
 
 
 if __name__ == "__main__":
-    import operator as o
-    it = o.itemgetter
-    print([it(*index)([1,2,3]) for index in generate_power_set([1,2,3])[1:]])
+    print(generate_balanced_parentheses(6))
